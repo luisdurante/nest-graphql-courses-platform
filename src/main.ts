815c941b5afc59c18eda6d160191b827cd8b4492
Luis Durante <luisdurante2@outlook.com>
2023-05-05 15:26:14 -0300

    :fire: Remove CORS

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
+
   await app.listen(process.env.PORT || 8080);
 }
 
